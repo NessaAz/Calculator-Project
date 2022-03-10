ed49262e7a2b9eab2aeccb485c0bfafa54e78d9a
vanta blanta <vantablanta@gmail.com>
2022-03-10 10:52:29 +0300

    call a jquery function to collect values click by user and store tham in a variable

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("span").on('click', function(){
+        var value = $(this).text()
+        console.log(value)
+        console.log(typeof value)
+    })
+
+
+
+
+
+
+
+})
